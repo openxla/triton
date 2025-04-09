4ad0de1f04cb71c65d172165c6ebbfc781607b26
Copybara <no-reply@google.com>
2025-04-09 15:02:17 +0200

    OpenXLA-specific changes.
    PiperOrigin-RevId: 745554821

--- third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/DotOpToLLVM/WGMMA.cpp
+++ third_party/nvidia/lib/TritonNVIDIAGPUToLLVM/DotOpToLLVM/WGMMA.cpp
@@ -180,6 +180,10 @@ DotOpMmaV3SmemLoader loadA(const LLVMTypeConverter *typeConverter,
   // warpgroup.
   Value warp =
       b.and_(rewriter.create<nvgpu::WarpIdOp>(loc), b.i32_val(0xFFFFFFFC));
+  // Workaround for a bug in ptxas 12.3 that cause a failure in
+  // test_core.py::test_dot. The shuffle will force the compiler to treat the
+  // value as uniform and prevent wrong optimizations.
+  warp = mlir::LLVM::NVIDIA::shuffleIdx(loc, rewriter, warp, 0);
   Value warpM = b.urem(warp, b.i32_val(wpt[0]));
   Value warpId = b.urem(warpM, b.i32_val(shapePerCTA[0] / instrShape[0]));
 
